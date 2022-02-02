5211445c6344524c8c57eb77bdcf64ee9f954be8
Tarek Alhamad <Tarek.alhamad@yahoo.com>
2022-02-02 01:39:58 +0100

    Added ApolloClient

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,19 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+import { ApolloClient, InMemoryCache } from "@apollo/client/core";
+import { createApolloProvider } from "@vue/apollo-option";
+
+const cache = new InMemoryCache();
+const apolloClient = new ApolloClient({
+  cache,
+  uri: "https://graphql.contentful.com/content/v1/spaces/jr8dcjlyvv5x/environments/master?access_token=Q-hSYHANyqni75kNixs1ulQmtiK9dECBcnHM469vXMc",
+});
+
+const apolloProvider = createApolloProvider({
+  defaultClient: apolloClient,
+});
+
+const app = createApp(App);
+app.use(apolloProvider);
+app.mount("#app");
